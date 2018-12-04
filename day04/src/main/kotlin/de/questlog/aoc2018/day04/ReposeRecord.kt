565ef9c45948f52daceeb2a679b78090c8baebf4
Questlog <questlog@rinjamin.de>
2018-12-04 22:37:31 +0100

    day04

---
+++ day04/src/main/kotlin/de/questlog/aoc2018/day04/ReposeRecord.kt
@@ -0,0 +1,79 @@
+package de.questlog.aoc2018.day04
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+class ReposeRecord {
+    fun sneak(input: String) : Pair<Int, Int> {
+
+        val lines = input.lines().sortedBy {
+            it.substringAfter("[").substringBefore("]")
+        }
+
+        var state = State.ShiftChange
+        var guardsById = HashMap<Int, Guard>()
+        var currentGuard : Guard = Guard(0, ArrayList())
+        var startSleepTime = 0
+
+        for(line in lines){
+            state = when {
+                line.endsWith("begins shift") -> State.ShiftChange
+                line.endsWith("falls asleep") -> State.FallsAsleep
+                else -> State.WakesUp
+            }
+
+            if(state == State.ShiftChange) {
+                val id = Regex("#(\\d+)").find(line)!!.groupValues[1].toInt()
+                currentGuard = guardsById.computeIfAbsent(id) { Guard(id, ArrayList()) }
+            }
+
+            if(state == State.FallsAsleep) {
+                startSleepTime = Regex(":(\\d+)]").find(line)!!.groupValues[1].toInt()
+                val hour = Regex("(\\d+):").find(line)!!.groupValues[1].toInt()
+                if(hour == 23)
+                    startSleepTime = 0
+            }
+
+            if (state == State.WakesUp) {
+                var stopSleepTime = Regex(":(\\d+)]").find(line)!!.groupValues[1].toInt()
+
+                for(i in startSleepTime until stopSleepTime){
+                    currentGuard.minutesAsleep.add(i.toInt())
+                }
+            }
+        }
+
+        val mostMinutes = guardsById.map { it.value }
+            .maxBy { it.minutesAsleep.size }
+        val bestMinute = mostMinutes?.minutesAsleep?.groupBy { it }?.maxBy { it.value.size }?.key
+        val result = (mostMinutes?.id ?: 0) * (bestMinute ?: 0)
+
+        var max = 0
+        var guardSameMinute = guardsById.values.first()
+        var theMinute = 0
+        for (guard in guardsById.values) {
+            val mostMintues = guard.minutesAsleep.groupBy { it }.maxBy { it.value.size }
+            if (mostMintues != null) {
+                val count = mostMintues.value.size
+                if(count > max){
+                    max = count
+                    guardSameMinute = guard
+                    theMinute = mostMintues.key
+                }
+            }
+        }
+
+        val resultPartTwo = guardSameMinute.id * theMinute
+
+        return Pair(result, resultPartTwo)
+    }
+}
+
+data class Guard(
+    val id: Int,
+    val minutesAsleep: ArrayList<Int>
+)
+
+enum class State{
+    ShiftChange, FallsAsleep, WakesUp
+}
---
+++ day04/src/test/kotlin/de/questlog/aoc2018/day04/ReposeRecordTest.kt
@@ -0,0 +1,41 @@
+package de.questlog.aoc2018.day04
+
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ReposeRecordTest {
+    private lateinit var puzzleInput : String
+
+    @Before
+    fun setUp(){
+        puzzleInput = this::class.java.classLoader.getResource("day04Input.txt").readText()
+    }
+
+    @Test
+    fun sneakTest(){
+        val input = "[1518-11-01 00:00] Guard #10 begins shift\n" +
+                "[1518-11-01 00:05] falls asleep\n" +
+                "[1518-11-01 00:25] wakes up\n" +
+                "[1518-11-01 00:30] falls asleep\n" +
+                "[1518-11-01 00:55] wakes up\n" +
+                "[1518-11-01 23:58] Guard #99 begins shift\n" +
+                "[1518-11-02 00:40] falls asleep\n" +
+                "[1518-11-02 00:50] wakes up\n" +
+                "[1518-11-03 00:05] Guard #10 begins shift\n" +
+                "[1518-11-03 00:24] falls asleep\n" +
+                "[1518-11-03 00:29] wakes up\n" +
+                "[1518-11-04 00:02] Guard #99 begins shift\n" +
+                "[1518-11-04 00:36] falls asleep\n" +
+                "[1518-11-04 00:46] wakes up\n" +
+                "[1518-11-05 00:03] Guard #99 begins shift\n" +
+                "[1518-11-05 00:45] falls asleep\n" +
+                "[1518-11-05 00:55] wakes up"
+        val reposeRecord = ReposeRecord()
+        var result = reposeRecord.sneak(input)
+        assertEquals(240, result.first)
+
+        result = reposeRecord.sneak(puzzleInput)
+        println(result)
+    }
+}
